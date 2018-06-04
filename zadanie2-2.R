bea3368c0f8fa68b2a9a4cdd5679fdff736f4585
kingaszewczyk <38748590+kingaszewczyk@users.noreply.github.com>
2018-06-04 22:29:33 +0200

    com

--- zadanie2-2.R
+++ zadanie2-2.R
@@ -1,2 +1,9 @@
-#zadanie 2-1.2
-rok <- c(kwartal_1, kwartal_2, kwartal_3, kwartal_4)
+#zadanie 2-2
+oblicz_godziny <- function(bieżący_rok, bieżący_dzień, bieżący_miesiąc, obsuwa)
+{
+  Godziny_W_Tym_Roku <- (((12 - bieżący_miesiąc) * 30 ) - (30 - bieżący_dzień)) * 24
+  Godziny_Przez_Wszystkie_Lata <- (2022 - bieżący_rok - 1 + obsuwa) * 365 * 24
+  Godziny_Na_Ostatnim_Roku <-  (6 * 30 * 24)
+  return (Godziny_W_Tym_Roku + Godziny_Przez_Wszystkie_Lata + Godziny_Na_Ostatnim_Roku)
+}
+oblicz_godziny(2018, 26, 04, 0)
